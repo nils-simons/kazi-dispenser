f23ab9a89b84729423b0cdf87b8fee7554b9bc84
Nils Simons <nilssimons707@gmail.com>
2023-11-19 23:02:55 +0100

    1.*

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const rpio = require('rpio');
+var gpio = require("onoff").Gpio;
 
-// GPIO pin configuration
-const stepPin = 11; // GPIO pin for stepping
-const directionPin = 12; // GPIO pin for direction
+//use GPIO pin numbers
+var stepPins = [12,11,13,15];
+var pinNumber = stepPins.length;
+var pins = [];
+var stepCounter = 0;
+var timeout = 0.01;
+var stepCount = 8;
 
-// Stepper motor configuration
-const stepsPerRevolution = 200;
+Seq = [];
+Seq[0] = [1,0,0,0];
+Seq[1] = [1,1,0,0];
+Seq[2] = [0,1,0,0];
+Seq[3] = [0,1,1,0];
+Seq[4] = [0,0,1,0];
+Seq[5] = [0,0,1,1];
+Seq[6] = [0,0,0,1];
+Seq[7] = [1,0,0,1];
 
-// Set up GPIO pins
-rpio.open(stepPin, rpio.OUTPUT, rpio.LOW);
-rpio.open(directionPin, rpio.OUTPUT, rpio.LOW);
-
-// Move the stepper motor
-function moveStepper() {
-  const rpm = 5; // Set the speed in revolutions per minute
-
-  // Set the direction (1 for clockwise, 0 for counterclockwise)
-  rpio.write(directionPin, rpio.HIGH);
-
-  // Calculate delay based on RPM
-  const delay = Math.floor((60 * 1000) / (stepsPerRevolution * rpm));
-
-  // Rotate 360 degrees (1 revolution)
-  for (let step = 0; step < stepsPerRevolution; step++) {
-    rpio.write(stepPin, rpio.HIGH);
-    rpio.msleep(delay);
-    rpio.write(stepPin, rpio.LOW);
-    rpio.msleep(delay);
-  }
-
-  console.log('Stepper motor moved 360 degrees');
+for(var i=0; i<pinNumber; i++){
+  pins[i] = new gpio(stepPins[i], 'out');
 }
 
-// Call the function to move the stepper motor
-moveStepper();
+var step = function(){
+  for(var pin = 0; pin<4; pin++){
+    if(Seq[stepCounter][pin] != 0){
+      pins[pin].writeSync(1);
+    }else{
+      pins[pin].writeSync(0);
+    }
+  }
+  stepCounter += 1
+  if (stepCounter==stepCount){
+    stepCounter = 0;
+  }
+  if (stepCounter<0){
+    stepCounter = stepCount;
+  }
+  setTimeout( function(){step()}, timeout );
+}
 
-// Close GPIO pins when done
-rpio.close(stepPin);
-rpio.close(directionPin);
+step();
